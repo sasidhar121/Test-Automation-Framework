faa0051d992dde8054f787f8f3e131b2f084da77
Sasidhar <sasidhargandikota10@gmail.com>
2024-12-29 12:17:18 +0530

    Removed unused import

--- src/test/java/com/ui/listeners/TestListener.java
+++ src/test/java/com/ui/listeners/TestListener.java
@@ -15,7 +15,6 @@ import com.ui.tests.TestBase;
 import com.utility.BrowserUtility;
 import com.utility.ExtentReporterUtility;
 import com.utility.LoggerUtlity;
-import com.utility.LoggerUtlity;
 
 public class TestListener implements ITestListener {
 	Logger logger = LoggerUtlity.getLogger(this.getClass());
