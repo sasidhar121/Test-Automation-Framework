a072d3fc8d1154dff4e8b6dc192a644423a19747
Sasidhar <sasidhargandikota10@gmail.com>
2024-12-29 15:18:59 +0530

    Rollbacked changes

--- src/test/java/com/utility/ExtentReporterUtility.java
+++ src/test/java/com/utility/ExtentReporterUtility.java
@@ -5,42 +5,29 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentReporterUtility {
-    private static ExtentReports extentReports; // Global ExtentReports instance
-    private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+	private static ExtentReports extentReports;
+	private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
 
-    // Set up the ExtentReports and SparkReporter
-    public static void setupSparkReporter(String reportName) {
-        if (extentReports == null) { // Ensure it's initialized only once
-            ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(
-                    System.getProperty("user.dir") + "/" + reportName);
+	public static void setupSparkReporter(String reportName) {
+		ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(
+				System.getProperty("user.dir") + "//" + reportName);
+		ExtentReports extentReports= new ExtentReports();
+		
 
-            extentReports = new ExtentReports();
-            extentReports.attachReporter(extentSparkReporter);
-        }
-    }
+		extentReports.attachReporter(extentSparkReporter);
+	}
 
-    // Create an ExtentTest for the current thread
-    public static void createExtentTest(String testName) {
-        if (extentReports == null) {
-            throw new IllegalStateException("ExtentReports is not initialized. Call setupSparkReporter() first.");
-        }
-        ExtentTest test = extentReports.createTest(testName);
-        extentTest.set(test);
-    }
+	public static void createExtentTest(String testName) {
+		ExtentTest test = extentReports.createTest(testName);
+		extentTest.set(test);
+	}
 
-    // Get the ExtentTest for the current thread
-    public static ExtentTest getTest() {
-        ExtentTest test = extentTest.get();
-        if (test == null) {
-            throw new IllegalStateException("No ExtentTest found for the current thread. Ensure createExtentTest() is called.");
-        }
-        return test;
-    }
+	public static ExtentTest getTest() {
+		return extentTest.get();
+	}
 
-    // Flush the ExtentReports to write results
-    public static void flushReport() {
-        if (extentReports != null) {
-            extentReports.flush();
-        }
-    }
+	public static void flushReport() {
+		extentReports.flush();
+	}
+
 }
